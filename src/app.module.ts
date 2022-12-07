fe91b233bb418a383d2b51c2685581ca2b436bd7
ziya <zibutsu143@gmail.com>
2022-12-07 11:17:38 +0500

    Validation Pipe added to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
@@ -7,6 +9,14 @@ import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [CatsModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
