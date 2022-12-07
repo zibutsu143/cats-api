268521fa5d37e6f0dfe9e4a705201f4e9d3ea5f5
ziya <zibutsu143@gmail.com>
2022-12-07 12:44:12 +0500

    unit test for cats service added

--- src/cats/cats.service.spec.ts
+++ src/cats/cats.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
 import { CatsService } from './cats.service';
+import { Cats, Prisma } from '@prisma/client';
+import { NotFoundException } from '@nestjs/common';
 
+// TODO : Decouple Prisma Service to create a fake service for testing
 describe('CatsService', () => {
   let service: CatsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CatsService],
+      providers: [CatsService, PrismaService],
     }).compile();
 
     service = module.get<CatsService>(CatsService);
@@ -14,5 +18,11 @@ describe('CatsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw not found error when updated with an invalid cat id ', async () => {
+    await expect(service.update(1000000, {})).rejects.toThrow(
+      NotFoundException,
+    );
   });
 });
